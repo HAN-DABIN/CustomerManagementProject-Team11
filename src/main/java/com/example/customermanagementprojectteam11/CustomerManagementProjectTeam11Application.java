9ce6ebd1fd845f0e43a456d8f9f3104845759166
HAN-DABON <sinmint11@gmail.com>
2026-04-23 15:52:10 +0900

    Initial commit

---
+++ src/main/java/com/example/customermanagementprojectteam11/CustomerManagementProjectTeam11Application.java
@@ -0,0 +1,13 @@
+package com.example.customermanagementprojectteam11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagementProjectTeam11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerManagementProjectTeam11Application.class, args);
+    }
+
+}
