6f03c71c2c9346da56855badc150f6915797303e
송이 <springday3355@gmail.com>
2026-04-24 14:41:46 +0900

    Entity 생성

---
+++ src/main/java/com/example/customermanagementprojectteam11/admin/entity/Admin.java
@@ -0,0 +1,47 @@
+package com.example.customermanagementprojectteam11.admin.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "admins")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Admin extends BaseEntity { //BaseEntity 상속
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 200) // 유니크(중복불가) 추가
+    private String email;
+
+    @Column(nullable = false, length = 100) // 비밀번호 길이 수정 50-> 100
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String role;
+
+    @Column(name = "phone_number",nullable = false, length = 20)
+    private String phone_number;
+
+    @Column(nullable = false,length = 20)
+    private String status;
+
+    private Admin(String name, String email,
+                  String password, String role,
+                   String phone_number ) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.phone_number = phone_number;
+        this.status = "승인 대기"; // 초기값 자동 설정
+    }
+
+}
---
+++ src/main/java/com/example/customermanagementprojectteam11/admin/entity/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.example.customermanagementprojectteam11.admin.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at", nullable = false)
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "approved_at")
+    private LocalDateTime approved_at;
+
+    // 서비스에서 사용
+    public void approve(){
+        this.approved_at = LocalDateTime.now();
+    }
+
+}
