c72916ad0645d21d23d04e6cac0d774dc24c5a04
HAN-DABON <sinmint11@gmail.com>
2026-04-29 11:24:59 +0900

    feat: order entity 생성

---
+++ src/main/java/com/example/customermanagementprojectteam11/order/entity/OrderItem.java
@@ -0,0 +1,37 @@
+package com.example.customermanagementprojectteam11.order.entity;
+
+import com.example.customermanagementprojectteam11.admin.entity.Admin;
+import com.example.customermanagementprojectteam11.customer.entity.Customer;
+import com.example.customermanagementprojectteam11.product.entity.Product;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "oreder_itmes")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    // customer엔티티와 연관관계 1:N
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "custormer_id")
+    private Customer customer;
+    // product엔티티와 연관관계 1:N
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+    // admin엔티티와 연관관계 1:N
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "admin_id")
+    private Admin admin;
+    @Column(nullable = false)
+    private Long quantity;
+    @Column(name = "total_price", nullable = false)
+    private Long totalPrice;
+
+
+}
