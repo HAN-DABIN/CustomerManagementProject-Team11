b90b40854f2783e7452b2d2b48a9b93227537d8c
HAN-DABON <sinmint11@gmail.com>
2026-04-29 19:29:55 +0900

    refactor: 로그인 시 암호화 비밀번호 검증 누락 추가

--- src/main/java/com/example/customermanagementprojectteam11/login/service/AdminLoginService.java
+++ src/main/java/com/example/customermanagementprojectteam11/login/service/AdminLoginService.java
@@ -1,5 +1,6 @@
 package com.example.customermanagementprojectteam11.login.service;
 
+import com.example.customermanagementprojectteam11.admin.config.PasswordEncoder;
 import com.example.customermanagementprojectteam11.admin.entity.Admin;
 import com.example.customermanagementprojectteam11.admin.repository.AdminRepository;
 import com.example.customermanagementprojectteam11.login.dto.LoginRequest;
@@ -12,20 +13,22 @@ import org.springframework.transaction.annotation.Transactional;
 public class AdminLoginService {
 
     private final AdminRepository adminRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional(readOnly = true)
     public Admin Login(LoginRequest request) {
         // 먼저 이메일 조회
         Admin admin = adminRepository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new IllegalArgumentException("이메일이 맞지않습니다."));
-        //비밀번호 조회하고
-        if (!admin.getPassword().equals(request.getPassword())) {
+        //비밀번호 조회하고 (암호화 된 비밀번호랑 입력된 비밀번호 검증)
+        if (!passwordEncoder.matches(request.getPassword(), admin.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 맞지않습니다.");
         }
         // Active 상태일 때만 로그인 가능
         if (!admin.canLogin()) {
             throw new IllegalStateException(admin.loginMessage());
         }
+
         //session.setAttribute("loginAdmin", admin); // 세션 인증
         return admin;
 
